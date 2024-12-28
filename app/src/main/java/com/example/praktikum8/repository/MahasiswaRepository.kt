08bc5ece25d08193680ed49e8f80e96bc7e38448
isro24 <alamat_email_anda@example.com>
2024-12-28 22:23:04 +0700

    Membuat Projek

---
+++ app/src/main/java/com/example/praktikum8/repository/MahasiswaRepository.kt
@@ -0,0 +1,51 @@
+package com.example.praktikum8.repository
+
+import com.example.praktikum8.model.Mahasiswa
+import com.example.praktikum8.service_api.MahasiswaService
+import okio.IOException
+
+interface MahasiswaRepository{
+    suspend fun insertMahasiswa(mahasiswa: Mahasiswa)
+
+    suspend fun getMahasiswa(): List<Mahasiswa>
+
+    suspend fun updateJdwl(nim: String, mahasiswa: Mahasiswa)
+
+    suspend fun deleteJdwl(nim: String)
+
+    suspend fun getMahasiswaByNim(nim: String): Mahasiswa
+}
+
+class NetworkKontakRepository(
+    private val kontakApiService: MahasiswaService
+): MahasiswaRepository{
+    override suspend fun insertMahasiswa(mahasiswa: Mahasiswa) {
+        kontakApiService.insertMahasiswa(mahasiswa)
+    }
+
+    override suspend fun updateJdwl(nim: String, mahasiswa: Mahasiswa) {
+        kontakApiService.updateMahasiswa(nim, mahasiswa)
+    }
+
+    override suspend fun deleteJdwl(nim: String) {
+        try {
+            val response = kontakApiService.deleteMahasiswa(nim)
+            if (!response.isSuccessful){
+                throw IOException("Failed to delete Kontak. HTTP Status code: " +
+                "${response.code()}")
+            }else{
+                response.message()
+                println(response.message())
+            }
+        }catch (e:Exception){
+            throw e
+        }
+    }
+
+    override suspend fun getMahasiswa(): List<Mahasiswa> =
+        kontakApiService.getAllMahasiswa()
+
+    override suspend fun getMahasiswaByNim(nim: String): Mahasiswa {
+        return kontakApiService.getMahasiswaByNim(nim)
+    }
+}
