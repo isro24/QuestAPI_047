1a45ff0ad9a9ada977f899e29652a37c3bb53653
isro24 <alamat_email_anda@example.com>
2024-12-28 22:55:28 +0700

    Menambah fun InsertUiEvent.toMhs, Mahasiswa.toUiStateMhs, dan Mahasiswa.toInsertUiEvent pada InsertViewModel

--- app/src/main/java/com/example/praktikum8/viewmodel/InsertViewModel.kt
+++ app/src/main/java/com/example/praktikum8/viewmodel/InsertViewModel.kt
@@ -5,6 +5,7 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.example.praktikum8.model.Mahasiswa
 import com.example.praktikum8.repository.MahasiswaRepository
 import kotlinx.coroutines.launch
 
@@ -12,8 +13,8 @@ class InsertViewModel(private val mhs: MahasiswaRepository):ViewModel(){
     var uiState by mutableStateOf(InsertUiState())
         private set
 
-    fun updateInsertMhsState(insertUiEvent.InsertUiEvent){
-        uiState = InsertUiState(insertUiEvent.insertUiEvent)
+    fun updateInsertMhsState(insertUiEvent: InsertUiEvent){
+        uiState = InsertUiState(insertUiEvent=insertUiEvent)
     }
 
     suspend fun insertMhs(){
@@ -38,5 +39,27 @@ data class InsertUiEvent(
     val jenisKelamin:String="",
     val kelas:String="",
     val angkatan:String="",
+)
+
+fun InsertUiEvent.toMhs(): Mahasiswa = Mahasiswa(
+    nim=nim,
+    nama=nama,
+    alamat=alamat,
+    jenisKelamin=jenisKelamin,
+    kelas=kelas,
+    angkatan=angkatan
+)
+
+fun Mahasiswa.toUiStateMhs():InsertUiState= InsertUiState(
+    insertUiEvent = toInsertUiEvent()
+)
+
+fun Mahasiswa.toInsertUiEvent(): InsertUiEvent=InsertUiEvent(
+    nim=nim,
+    nama=nama,
+    alamat=alamat,
+    jenisKelamin=jenisKelamin,
+    kelas=kelas,
+    angkatan=angkatan
 )
 
