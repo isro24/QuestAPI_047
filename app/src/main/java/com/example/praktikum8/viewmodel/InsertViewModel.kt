9a59fa44e6f9d02009f6d87b5521044dd250e06f
isro24 <alamat_email_anda@example.com>
2024-12-28 22:47:52 +0700

    Menambah class InsertViewModel dan data classnya

---
+++ app/src/main/java/com/example/praktikum8/viewmodel/InsertViewModel.kt
@@ -0,0 +1,42 @@
+package com.example.praktikum8.viewmodel
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.praktikum8.repository.MahasiswaRepository
+import kotlinx.coroutines.launch
+
+class InsertViewModel(private val mhs: MahasiswaRepository):ViewModel(){
+    var uiState by mutableStateOf(InsertUiState())
+        private set
+
+    fun updateInsertMhsState(insertUiEvent.InsertUiEvent){
+        uiState = InsertUiState(insertUiEvent.insertUiEvent)
+    }
+
+    suspend fun insertMhs(){
+        viewModelScope.launch {
+            try {
+                mhs.insertMahasiswa(uiState.insertUiEvent.toMhs())
+            }catch (e:Exception){
+                e.printStackTrace()
+            }
+        }
+    }
+}
+
+data class InsertUiState(
+    val insertUiEvent: InsertUiEvent = InsertUiEvent()
+)
+
+data class InsertUiEvent(
+    val nim:String="",
+    val nama:String="",
+    val alamat:String="",
+    val jenisKelamin:String="",
+    val kelas:String="",
+    val angkatan:String="",
+)
+
