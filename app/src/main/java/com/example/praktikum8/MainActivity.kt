bb4755e2b65f3af1b4e98492a5a1ef57bb446641
isro24 <alamat_email_anda@example.com>
2024-12-30 10:12:08 +0700

    Menambah modifier pada MahasiswaApp dan MainActivity

--- app/src/main/java/com/example/praktikum8/MainActivity.kt
+++ app/src/main/java/com/example/praktikum8/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -19,8 +20,10 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             Praktikum8Theme {
-                Scaffold(modifier = Modifier.fillMaxSize()) {
-                    MahasiswaApp()
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    MahasiswaApp(
+                        modifier = Modifier.padding(innerPadding)
+                    )
                 }
             }
         }
